a92cc1a093565b7762d6beb0d9ba56fd9c21c1a8
ChandarAwesome <ajaygames00Gmail.com>
2023-01-20 18:05:57 +0530

    [Chandra]-Push

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace StackUsingLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello World!");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+            //linkedListStack.Peek();
+            //linkedListStack.Pop();
+            //linkedListStack.IsEmpty();
+            //linkedListStack.Display();
+            Console.ReadKey();
+        }
+    }
+}
